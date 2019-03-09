a26a3b7db1479b70f831133defdb708f17768afd
Youthink <hufy3651@foxmail.com>
2019-03-09 08:23:18 +0800

    umijs init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+
+import './app.scss';
+import './index.scss';
+
+export default function() {
+  return (
+    <div className="app">
+      <div className="box-container">
+        <header className="site-header">
+          这里写网站的名称
+        </header>
+      </div>
+    </div>
+  );
+}
