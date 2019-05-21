553bc8dda5ffd52cd447f8cd83d4e12c193fcec1
Youthink <hufy3651@foxmail.com>
2019-05-22 01:09:31 +0800

    basic page

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,27 @@ export default function() {
     <div className="app">
       <div className="box-container">
         <header className="site-header">
-          这里写网站的名称
+          <div className="box">
+            <div className="left">
+              这里写网站的名称
+            </div>
+            <div className="right">
+              这里留个登录<br />
+              登录显示弹窗，展示第三方授权方式
+            </div>
+          </div>
+
+          <div className="box">
+            <div className="box-center">
+              if 用户未登录 显示网站使用说明和目前的人数
+            </div>
+          </div>
+
+          <div className="box">
+            <div className="box-center">
+              if 用户登录 显示以下内容
+            </div>
+          </div>
         </header>
       </div>
     </div>
