5619c8a31fcb23dd28dafd66e28fac21349d561b
Youthink <hufy3651@foxmail.com>
2019-05-22 23:42:05 +0800

    antd && sass && react

--- .umirc.js
+++ .umirc.js
@@ -1,8 +1,14 @@
 export default {
   base: '/ihabit/',
   publicPath: '/ihabit/',
+  sass: {},
   disableCSSModules: true,
   plugins: [
     ['umi-plugin-gh-pages'],
+    ['umi-plugin-react',
+      {
+        antd: true
+      }
+    ]
   ],
 }
