63fc1f3571d04eb1ed324b437135b6023d229206
Youthink <hufy3651@foxmail.com>
2019-05-23 09:18:15 +0800

    @ant-design webpack icon

--- .umirc.js
+++ .umirc.js
@@ -3,6 +3,14 @@ export default {
   publicPath: '/ihabit/',
   sass: {},
   disableCSSModules: true,
+  chainWebpack(config, { webpack }) {
+    // code split @ant-design/icons
+    config.module
+    .rule('@ant-design/icons')
+    .include.add(require.resolve('@ant-design/icons/lib/dist')).end()
+    .use('ant-icon')
+    .loader('webpack-ant-icon-loader');
+  },
   plugins: [
     ['umi-plugin-gh-pages'],
     ['umi-plugin-react',
