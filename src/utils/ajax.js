78e8976ff19dfe18bfd8959b9c60f01379b38d26
Youthink <hufy3651@foxmail.com>
2019-05-26 21:17:22 +0800

    fix api error message

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -1,4 +1,5 @@
 import request, { extend } from 'umi-request';
+import { message }         from 'antd';
 import apiUrl              from '@/utils/apiUrl';
 
 const ajax = extend({
@@ -8,10 +9,15 @@ const ajax = extend({
 });
 
 request.interceptors.response.use(async (response) => {
-  const data = await response.clone().json();
-  if(data && data.apiCode === '1000') {
+  const res = await response.clone().json();
+  if(res && res.apiCode === '1000') {
     console.log('需要登录');
   }
+
+  if(res && res.apiMessage && !res.success) {
+    return message.error(data.apiMessage);
+  }
+
   return response;
 })
 
